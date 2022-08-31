484a4ef2902dba3ddc496f617da469294a93466d
Tim Jenness <tjenness@lsst.org>
2022-08-31 15:15:23 -0700

    Use a more common location for cloc

--- bin/countlines.py
+++ bin/countlines.py
@@ -25,7 +25,7 @@ import sys
 import subprocess
 
 # Set to the location of the cloc.pl executable
-CLOC_EXE = "/Users/timj/Downloads/cloc-1.88.pl"
+CLOC_EXE = "/opt/homebrew/bin/cloc"
 
 # We cannot guess tags so easiest to list them
 # eg in lsst_distrib git repo: git tag | grep w.
